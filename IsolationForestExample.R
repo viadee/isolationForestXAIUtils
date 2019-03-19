d3caf22d2b44a13c8897dddf92df8b05a07ed663
Caroline Methner <Caroline.Methner@viadee.de>
2019-03-19 12:40:29 +0100

    Isolation Forest XAI-Approach

---
+++ IsolationForestExample.R
@@ -0,0 +1,43 @@
+# 1. load data and packages
+library(h2o)
+library(dplyr)
+library(ggplot2)
+source("./XAIUtils.R") # library to explain isolationForest
+titanic = read.csv("./titanic.csv")
+
+# 2. generate train and test data
+train = titanic[1:round(nrow(titanic)*0.8),]
+test = titanic[round(nrow(titanic)*0.8):nrow(titanic),]
+
+h2o.init()
+trainh2o = as.h2o(train[,c(-1,-2,-3,-12)]) # remove useless columns (e.g. IDs)
+testh2o = as.h2o(test[,c(-1,-2,-3,-12)])
+
+
+# 3. train model
+model_iso <- h2o.isolationForest(training_frame = trainh2o, 
+                                 model_id = "isoForest", 
+                                 seed = 42, ntrees = 100)
+
+iso_DF = h2o.predict(model_iso, testh2o) %>% as.data.frame()
+
+# 4. identify anomalies
+anomaly_threshold = 5.5 # Set threshold by looking at the graph!
+iso_DF_ordered = iso_DF %>% arrange(desc(mean_length))
+ggplot(iso_DF_ordered, aes(x=c(1:nrow(iso_DF_ordered)), y=iso_DF_ordered$mean_length)) +
+  geom_point(size=2, shape=1) + ylab("Mean Length") + xlab(" ") + geom_hline(yintercept = anomaly_threshold) + theme(legend.position="none")
+
+
+iso_anomaly = data.frame(iso_DF,test) %>% filter(mean_length < anomaly_threshold)
+
+# 5. Explain anomalies
+findShortestPath(model_iso, iso_anomaly[1,])
+findShortestPath(model_iso, iso_anomaly[2,])
+findShortestPath(model_iso, iso_anomaly[3,])
+findShortestPath(model_iso, iso_anomaly[4,])
+findShortestPath(model_iso, iso_anomaly[5,])
+findShortestPath(model_iso, iso_anomaly[6,])
+findShortestPath(model_iso, iso_anomaly[7,])
+
+
+train %>% filter(Survived < 0.5) %>% filter(SibSp > 6.5)
---
+++ XAIUtils.R
@@ -0,0 +1,95 @@
+# recursive function to traverse all nodes of one tree for a specific anomaly and return path
+recursiveTree <- function(rootnode, feature, threshold, documentation, anomaly){
+  
+  anomalyFeature <- as.numeric(anomaly[colnames(anomaly) == feature])
+  eq = ifelse(threshold < anomalyFeature,">=","<")
+  documentationNew <- data.frame(Feature = as.character(feature), Eq = eq, Threshold = threshold, stringsAsFactors = FALSE)
+  documentationNew = rbind(documentation,documentationNew)
+  
+
+  # Case 1: left node
+  if(is.na(threshold)){
+    newnode = rootnode@left_child
+    if(class(newnode)[1] != "H2OLeafNode"){ # stop if node is leaf node
+      newfeature = rootnode@left_child@split_feature
+      newthreshold = rootnode@left_child@threshold
+      recursiveTree(newnode,newfeature,newthreshold,documentationNew,anomaly)
+    }else{
+      return(documentationNew)
+    }
+  }# Case 2: right node
+  
+  else if(is.na(anomalyFeature) || anomalyFeature >= threshold ){
+    newnode = rootnode@right_child
+    if(class(newnode)[1] != "H2OLeafNode"){ #stop if node is leaf node
+      newfeature = rootnode@right_child@split_feature
+      newthreshold = rootnode@right_child@threshold
+      recursiveTree(newnode,newfeature,newthreshold,documentationNew,anomaly)
+    }else{
+      return(documentationNew)
+    }
+    # Case 3: left node
+  }else{
+    newnode = rootnode@left_child
+    if(class(newnode)[1] != "H2OLeafNode"){ # stop if node is leaf node
+      newfeature = rootnode@left_child@split_feature
+      newthreshold = rootnode@left_child@threshold
+      recursiveTree(newnode,newfeature,newthreshold,documentationNew,anomaly)
+    }else{
+      return(documentationNew)
+    }
+  }
+}
+
+# call recursive function that traverses tree for a specific anomaly
+traverseTree = function(anomaly,isoTree){
+  options(scipen = 999)
+  documentation <- data.frame(Feature=character(), Eq = character(), Threshold = double(),stringsAsFactors=FALSE)
+  root = isoTree@root_node
+  feature = root@split_feature
+  threshold = root@threshold
+  return(recursiveTree(root,feature,threshold,documentation,anomaly))
+}
+
+
+# find shortest path of isolation forest
+# if there are multiple shortest paths return the last one
+findShortestPath = function(isoForest,anomaly){
+  tree = h2o.getModelTree(model = isoForest, tree_number = 1)
+  shortestPath = traverseTree(anomaly,tree)
+  numberOfCriteria = nrow(shortestPath)
+  
+  for(i in 2:100){
+    tree = h2o.getModelTree(model = isoForest, tree_number = i)
+    if(numberOfCriteria >= nrow(traverseTree(anomaly,tree))){
+      shortestPath = traverseTree(anomaly,tree)
+      numberOfCriteria = nrow(traverseTree(anomaly,tree))
+    }
+  }
+  return(shortestPath)
+}
+
+
+# if there are multiple shortest paths (with same length)
+# find all of them (including their split features and thresholds)
+findShortestPaths = function(isoForest,anomaly){
+  tree = h2o.getModelTree(model = isoForest, tree_number = 1)
+  shortestPath = traverseTree(anomaly,tree)
+  numberOfCriteria = nrow(shortestPath)
+  
+  for(i in 2:100){
+    tree = h2o.getModelTree(model = isoForest, tree_number = i)
+    if(numberOfCriteria >= nrow(traverseTree(anomaly,tree))){
+      shortestPath = traverseTree(anomaly,tree)
+      numberOfCriteria = nrow(traverseTree(anomaly,tree))
+    }
+  }
+  
+  for(i in 1:100){
+    tree = h2o.getModelTree(model = isoForest, tree_number = i)
+    path = traverseTree(anomaly,tree)
+    if(numberOfCriteria == nrow(path)){
+      return(path)
+    }
+  }
+}
